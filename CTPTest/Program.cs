fa0cd8bfdcef0a0c862bdae31e8a9f3406d20dbf
kasicass <kasicass@gmail.com>
2017-08-22 11:51:18 +0800

    add more interface

--- CTPTest/Program.cs
+++ CTPTest/Program.cs
@@ -8,6 +8,12 @@ namespace CTPTest
         static void Main(string[] args)
         {
             Console.WriteLine("version: {0}", CThostFtdcMdApiWrapper.GetApiVersion());
+
+            CThostFtdcMdApiWrapper mdapi = new CThostFtdcMdApiWrapper("", false, false);
+            mdapi.Init();
+            Console.WriteLine("trading day: {0}", mdapi.GetTradingDay());
+            mdapi.Join();
+            mdapi.Release();
         }
     }
 }
--- CTPWrapper/CTPWrapper.h
+++ CTPWrapper/CTPWrapper.h
@@ -1,4 +1,5 @@
 // CTPWrapper.h
+// https://www.codeproject.com/articles/19354/quick-c-cli-learn-c-cli-in-less-than-minutes
 
 #pragma once
 
@@ -10,15 +11,42 @@ namespace CTPWrapper {
 	public ref class CThostFtdcMdApiWrapper
 	{
 	public:
-		static System::String^ GetApiVersion()
+		static String^ GetApiVersion()
 		{
-			return gcnew System::String(CThostFtdcMdApi::GetApiVersion());
+			return gcnew String(CThostFtdcMdApi::GetApiVersion());
 		}
 
 	public:
-		CThostFtdcMdApiWrapper()
+		CThostFtdcMdApiWrapper(String^ flowPath, bool bIsUsingUdp, bool bIsMulticast)
 		{
+			impl_ = CThostFtdcMdApi::CreateFtdcMdApi(
+				(char*)Runtime::InteropServices::Marshal::StringToHGlobalAnsi(flowPath).ToPointer(),
+				bIsUsingUdp,
+				bIsMulticast);
+		}
 
+		~CThostFtdcMdApiWrapper()
+		{
+		}
+
+		void Release()
+		{
+			impl_->Release();
+		}
+
+		void Init()
+		{
+			impl_->Init();
+		}
+
+		void Join()
+		{
+			impl_->Join();
+		}
+
+		String^ GetTradingDay()
+		{
+			return gcnew String(impl_->GetTradingDay());
 		}
 
 	private:
