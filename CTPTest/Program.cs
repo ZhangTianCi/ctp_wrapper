bb0d3daed0981c0e474ad246d293ab149c46de27
kasicass <kasicass@gmail.com>
2017-08-22 11:27:06 +0800

    test ok

---
+++ CTPTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using CTPWrapper;
+
+namespace CTPTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("version: {0}", CThostFtdcMdApiWrapper.GetApiVersion());
+        }
+    }
+}
